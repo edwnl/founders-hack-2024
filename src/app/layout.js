d3d307487802793e02e4def58921470931a26755
Ed <edwinli123456@gmail.com>
2024-08-02 17:10:19 +1000

    clean up unused imports

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,16 +1,13 @@
 import { Montserrat } from "next/font/google";
 import "./globals.css";
 import "antd/dist/reset.css";
-import { ConfigProvider } from "antd";
 import ClientLayout from "@/components/ClientLayout";
-import themeConfig from "@/theme/themeConfig";
-import NavBar from "@/components/NavBar";
 
 const montserrat = Montserrat({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Meetix",
-  description: "Ticketing with friends.",
+  description: "Where events meet connections.",
 };
 
 export default function RootLayout({ children }) {
