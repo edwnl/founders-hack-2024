6cd16c75765eb6dd8b5bbb24f2cb1a84cb237731
vitez04 <victor.tian360@gmail.com>
2024-08-04 02:36:39 +1000

    Backend for matchmaker/chats done

--- firebase/config.js
+++ firebase/config.js
@@ -1,6 +1,8 @@
+// firebase/config.js
 import { initializeApp, getApps } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_API_KEY,
@@ -9,6 +11,7 @@ const firebaseConfig = {
   storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_APP_ID,
+  databaseURL: process.env.NEXT_PUBLIC_DATABASE_URL,
 };
 
 // Initialize Firebase
@@ -21,5 +24,6 @@ if (!getApps().length) {
 
 const auth = getAuth(app);
 const db = getFirestore(app);
+const rtdb = getDatabase(app); 
 
-export { auth, db };
+export { auth, db, rtdb };
