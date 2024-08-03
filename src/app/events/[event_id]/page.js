d4f89cd6cc654f6ae017b68d93b85f4af07576e5
Ed <edwinli123456@gmail.com>
2024-08-04 01:37:23 +1000

    move view page and ticking buying logic to actions.js

--- src/app/events/[event_id]/page.js
+++ src/app/events/[event_id]/page.js
@@ -29,8 +29,7 @@ import Image from "next/image";
 import dayjs from "dayjs";
 import { withGuard } from "@/components/GuardRoute";
 import { useAuth } from "@/contexts/AuthContext";
-import { db } from "../../../../firebase/config";
-import { collection, doc, getDoc, updateDoc, increment } from "firebase/firestore";
+import { buyTickets, fetchEvent } from "@/app/events/[event_id]/actions";
 
 function EventDetails() {
   const [event, setEvent] = useState(null);
@@ -55,25 +54,7 @@ function EventDetails() {
   }
 
   useEffect(() => {
-    const fetchEvent = async () => {
-      try {
-        const eventRef = doc(db, "event", event_id);
-        const eventSnap = await getDoc(eventRef);
-
-        if (eventSnap.exists()) {
-          setEvent(eventSnap.data());
-        } else {
-          message.error("Event not found");
-        }
-      } catch (error) {
-        message.error("Error fetching event data");
-        console.error("Error: ", error);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchEvent();
+    getEventData();
   }, [event_id]);
 
   const showModal = () => setIsModalVisible(true);
@@ -91,26 +72,21 @@ function EventDetails() {
         // Simulating payment process
         await new Promise((resolve) => setTimeout(resolve, 1000));
 
-        // Update user's tickets
-        const userRef = doc(db, "users", user.uid);
-        await updateDoc(userRef, {
-          [`tickets.${event_id}`]: increment(ticketQuantity),
-        });
+        const result = await buyTickets(event_id, user.uid, ticketQuantity);
 
-        // Update event attendees
-        const eventRef = doc(db, "event", event_id);
-        await updateDoc(eventRef, {
-          attendees: increment(ticketQuantity),
-        });
-
-        message.success(`Successfully purchased ${ticketQuantity} ticket(s)!`);
-        setCheckoutStep(2);
+        if (result.success) {
+          message.success(result.message);
+          setCheckoutStep(2);
+        } else {
+          message.error(result.error);
+        }
       } catch (error) {
         message.error("Error processing the transaction.");
         console.error("Error: ", error);
       }
     }
   };
+
   const goToMatchmaking = () => {
     router.push(`/matchmaker/${event_id}`);
   };
